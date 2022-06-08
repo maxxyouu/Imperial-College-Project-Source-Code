1a6af7d771d2d140b2bc8cf2d2170670d70b348c
maxxyouu <max.you@outlook.com>
2022-06-08 18:02:50 +0100

    cam works after pass in one cam at a time instead of a whole list of cam

--- AttentionMap.py
+++ AttentionMap.py
@@ -32,42 +32,60 @@ if __name__ == '__main__':
     ))
 
     # for each image, it has a folder that store all the cam heatmaps
-    dataloader = DataLoader(data, batch_size=128)
-    cams = ['gradcam', 'gradcam++', 'scorecam', 'ablationcam', 'xgradcam', 'eigencam', 'fullgradcam']
+    dataloader = DataLoader(data, batch_size=len(data))
+    cams = ['eigencam'] # 'scorecam', 'ablationcam', 'xgradcam', 'eigencam',
+ 
+    x, _ = next(iter(dataloader))
+    for cam_name in cams:
+        cam = switch_cam(cam_name, resnet18.model, [resnet18_target_layer]) 
+        print('--------- Forward Passing {}'.format(cam_name))
+        grayscale_cam = cam(input_tensor=x, targets=None)
+        
+        # denormalize the image NOTE: must be placed after forward passing
+        x.mul_(Constants.DATA_STD).add_(Constants.DATA_MEAN)
+        
+        # for each image in a batch
+        for i in range(x.shape[0]):
+            # create directory this image-i if needed
+            dest = os.path.join(Constants.STORAGE_PATH, 'heatmaps', model_name, 'image-{}'.format(i))
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+                # save the original image
+                torchvision.utils.save_image(x[i, :], os.path.join(dest, 'original.jpg'))
 
-    # for x, _ in dataloader:
-    #     #normalize the image
-    #     x.mul_(Constants.DATA_STD).add_(Constants.DATA_MEAN)
-    #     for cam_name in cams:
-    #         cam = switch_cam(cam_name, resnet18.model, [resnet18_target_layer]) 
-    #         print('--------- Forward Passing {}'.format(cam_name))
-    #         grayscale_cam = cam(input_tensor=x, targets=None)
-            
-    #         # for each image in a batch
-    #         for i in range(x.shape[0]):
+            # swap the axis so that the show_cam_on_image works
+            img = x[i, :].cpu().detach().numpy()
+            img = np.swapaxes(img, 0, 2)
+            img = np.swapaxes(img, 0, 1)
+
+            # save the overlayed-attention map with the cam name as a tag
+            attention_map = show_cam_on_image(img, grayscale_cam[i, :], use_rgb=True)
+            masked_img = Image.fromarray(attention_map, 'RGB')
+            masked_img.save(os.path.join(dest, '{}.jpg'.format(cam_name)))
+
 
     # create directory for each image if not exists in directory eg. ./heatmaps/resnet/image-0/original.jpg
-    x, _ = next(iter(dataloader))
-    cam = GradCAM(model=resnet18.model, target_layers=[resnet18_target_layer], use_cuda=True if Constants.WORK_ENV == 'COLAB' else False)
-    grayscale_cam = cam(input_tensor=x, targets=None)
-    # denormalize after forward passing
-    x.mul_(Constants.DATA_STD).add_(Constants.DATA_MEAN)
+    # x, _ = next(iter(dataloader))
+    # cam = GradCAM(model=resnet18.model, target_layers=[resnet18_target_layer], use_cuda=True if Constants.WORK_ENV == 'COLAB' else False)
+    # grayscale_cam = cam(input_tensor=x, targets=None)
+    # # denormalize after forward passing
+    # x.mul_(Constants.DATA_STD).add_(Constants.DATA_MEAN)
 
-    dest = os.path.join(Constants.STORAGE_PATH, 'heatmaps', model_name, 'image-{}'.format(0))
-    if not os.path.exists(dest):
-        os.makedirs(dest)
-    # save the original image
-    torchvision.utils.save_image(x[0, :], os.path.join(dest, 'original.jpg'))
+    # dest = os.path.join(Constants.STORAGE_PATH, 'heatmaps', model_name, 'image-{}'.format(0))
+    # if not os.path.exists(dest):
+    #     os.makedirs(dest)
+    # # save the original image
+    # torchvision.utils.save_image(x[0, :], os.path.join(dest, 'original.jpg'))
 
-    # swap the axis so that the show_cam_on_image works
-    img = x[0, :].cpu().detach().numpy()
-    img = np.swapaxes(img, 0, 2)
-    img = np.swapaxes(img, 0, 1)
+    # # swap the axis so that the show_cam_on_image works
+    # img = x[0, :].cpu().detach().numpy()
+    # img = np.swapaxes(img, 0, 2)
+    # img = np.swapaxes(img, 0, 1)
 
-    # save the overlayed-attention map with the cam name as a tag
-    attention_map = show_cam_on_image(img, grayscale_cam[0, :], use_rgb=True)
-    masked_img = Image.fromarray(attention_map, 'RGB')
-    # masked_img.save(os.path.join(dest, '{}.jpg'.format(cam_name)))
-    masked_img.save(os.path.join(dest, 'grad-cam.jpg'))
+    # # save the overlayed-attention map with the cam name as a tag
+    # attention_map = show_cam_on_image(img, grayscale_cam[0, :], use_rgb=True)
+    # masked_img = Image.fromarray(attention_map, 'RGB')
+    # # masked_img.save(os.path.join(dest, '{}.jpg'.format(cam_name)))
+    # masked_img.save(os.path.join(dest, 'grad-cam.jpg'))
 
 

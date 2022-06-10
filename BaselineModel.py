a8120987129a74832980160dbcdfde2de3ef77e5
maxxyouu <max.you@outlook.com>
2022-06-10 13:49:07 +0100

    use the timm library instead as the model zoo platform

--- BaselineModel.py
+++ BaselineModel.py
@@ -1,4 +1,5 @@
 import torch
+import timm
 import torch.nn as nn
 import Constants
 
@@ -8,8 +9,8 @@ class Baseline_Model:
     https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
     """
     def __init__(self, pretrain=False, model_name='resnet18') -> None:
-        self.model = torch.hub.load('pytorch/vision:v0.10.0', model_name, pretrained=pretrain)
-
+        # self.model = torch.hub.load('pytorch/vision:v0.10.0', model_name, pretrained=pretrain)
+        self.model = timm.create_model(model_name, pretrained=pretrain)
         # modify the model that suit our task, ie: the output layer and etc
 
     def _custom_classifier(self):
@@ -50,11 +51,17 @@ class Pytorch_default_vgg(Baseline_Model):
         # modify the model to match our dataset with two class only
         self.model.classifier[6] = nn.Linear(self.model.classifier[6].in_features, num_classes, device=device, dtype=dtype)
 
+class Pytorch_default_skres(Baseline_Model):
+
+    def __init__(self, dtype, device, num_classes=2, pretrain=False, model_name='skresnet18') -> None:
+        super().__init__(pretrain, model_name)
+        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes, device=device, dtype=dtype)
+        
 
 if __name__ == '__main__':
     # print(torch. __version__)
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+    # model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
 
-    # net = Pytorch_default_vgg(dtype=Constants.DTYPE, device=Constants.DEVICE)
+    net = Pytorch_default_skres(dtype=Constants.DTYPE, device=Constants.DEVICE)
     # net.view_model()
     print("hello")

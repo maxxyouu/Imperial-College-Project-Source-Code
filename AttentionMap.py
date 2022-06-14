3dc14cd6a21088fdacd06ebd92c1ff8b19fcf0ca
maxxyouu <max.you@outlook.com>
2022-06-14 18:57:19 +0100

    attention map that generate all cams inside a image directory

--- AttentionMap.py
+++ AttentionMap.py
@@ -8,6 +8,7 @@ from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 import numpy as np
 import os
 import torchvision
+from torch.utils.data.sampler import SequentialSampler
 import torch
 from PIL import Image
 import shutil
@@ -96,66 +97,71 @@ def find_mutual_correct_images(dest_root):
 if __name__ == '__main__':
 
     # get all mutual correct predictions
-    find_mutual_correct_images(os.path.join(Constants.STORAGE_PATH, 'mutual_corrects'))
+    # find_mutual_correct_images(os.path.join(Constants.STORAGE_PATH, 'mutual_corrects'))
 
-    # args = extract_attention_cam_args()
+    args = extract_attention_cam_args()
 
-    # model_wrapper = get_trained_model(args.model)
-    # model_target_layer = target_layers(model_wrapper.model, args.layers)
+    model_wrapper = get_trained_model(args.model)
+    model_target_layer = target_layers(model_wrapper.model, args.layers)
 
-    # model_dir_name = define_model_dir_path(args)
+    model_dir_name = define_model_dir_path(args)
 
-    # data_dir = os.path.join(Constants.STORAGE_PATH, 'correct_preds', args.model)
-    # data = datasets.ImageFolder(data_dir, transform=transforms.Compose(
-    #     [
-    #         transforms.ToTensor(), # no need for the centercrop as it is at the cor
-    #         transforms.Normalize(
-    #             [Constants.DATA_MEAN, Constants.DATA_MEAN, Constants.DATA_MEAN], 
-    #             [Constants.DATA_STD,Constants.DATA_STD, Constants.DATA_STD])
-    #     ]
-    # ))
+    data_dir = os.path.join(Constants.STORAGE_PATH, 'correct_preds', args.model)
+    data = datasets.ImageFolder(data_dir, transform=transforms.Compose(
+        [
+            transforms.ToTensor(), # no need for the centercrop as it is at the cor
+            transforms.Normalize(
+                [Constants.DATA_MEAN, Constants.DATA_MEAN, Constants.DATA_MEAN], 
+                [Constants.DATA_STD,Constants.DATA_STD, Constants.DATA_STD])
+        ]
+    ))
 
     # # for each image, it has a folder that store all the cam heatmaps
-    # dataloader = DataLoader(data, batch_size=args.batchSize) # TODO: check image 18
-
-    # cam = switch_cam(args.cam, model_wrapper.model, model_target_layer) 
-    # for x, y in dataloader:
-    #     sample_name = ''
-    #     print('--------- Forward Passing {}'.format(args.cam))
-    #     x = x.to(device=Constants.DEVICE, dtype=Constants.DTYPE)
-    #     input_x = x
-    #     if args.noiseSmooth:
-    #         grayscale_cam = torch.zeros((x.shape[0], x.shape[-1], x.shape[-1]),dtype=Constants.DTYPE)
-    #         for t in range(args.iterations):
-    #             print('CAM Smoothing Iteration: {}'.format(t))
-    #             input_x = add_noise(x, args.std)
-    #             grayscale_cam += cam(input_tensor=input_x, targets=None)
-    #         grayscale_cam /= args.iterations
-    #     else:
-    #         grayscale_cam = cam(input_tensor=input_x, targets=None)
+    dataloader = DataLoader(data, batch_size=args.batchSize, sampler=SequentialSampler) # TODO: check image 18
+    image_order_book, img_index = data.imgs, 0
+    cam = switch_cam(args.cam, model_wrapper.model, model_target_layer)
+    for x, y in dataloader:
+        # NOTE: make sure i able index to the correct index
+        print('--------- Forward Passing {}'.format(args.cam))
+        x = x.to(device=Constants.DEVICE, dtype=Constants.DTYPE)
+        input_x = x
+        if args.noiseSmooth:
+            grayscale_cam = torch.zeros((x.shape[0], x.shape[-1], x.shape[-1]),dtype=Constants.DTYPE)
+            for t in range(args.iterations):
+                print('CAM Smoothing Iteration: {}'.format(t))
+                input_x = add_noise(x, args.std)
+                grayscale_cam += cam(input_tensor=input_x, targets=None)
+            grayscale_cam /= args.iterations
+        else:
+            grayscale_cam = cam(input_tensor=input_x, targets=None)
         
-    #     # denormalize the image NOTE: must be placed after forward passing
-    #     x = denorm(x)
+        # denormalize the image NOTE: must be placed after forward passing
+        x = denorm(x)
         
-    #     print('--------- Generating CAM')
-    #     # for each image in a batch
-    #     for i in range(x.shape[0]):
-    #         # create directory this image-i if needed that contains all the cam attention maps
-    #         dest = os.path.join(Constants.STORAGE_PATH, 'heatmaps', model_dir_name, '0' if y[i].item() == 0 else '1', sample_name[i][:-4])
+        print('--------- Generating CAM')
+        # for each image in a batch
+        for i in range(x.shape[0]):
+            sample_name = image_order_book[0][0].split('/')[-1] # get the image name from the dataset
 
-    #         # save the original image in parallel
-    #         if not os.path.exists(dest):
-    #             os.makedirs(dest)
-    #             # save the original image
-    #             torchvision.utils.save_image(x[i, :], os.path.join(dest, 'original.jpg'))
+            # each image is a directory that contains all the experiment results
+            dest = os.path.join(Constants.STORAGE_PATH, 'heatmaps', model_dir_name, '0' if y[i].item() == 0 else '1', sample_name)
 
-    #         # swap the axis so that the show_cam_on_image works
-    #         img = x[i, :].cpu().detach().numpy()
-    #         img = np.swapaxes(img, 0, 2)
-    #         img = np.swapaxes(img, 0, 1)
+            # save the original image in parallel
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+                # save the original image
+                torchvision.utils.save_image(x[i, :], os.path.join(dest, 'original.jpg'))
 
-    #         # save the overlayed-attention map with the cam name as a tag
-    #         attention_map = show_cam_on_image(img, grayscale_cam[i, :], use_rgb=True)
-    #         masked_img = Image.fromarray(attention_map, 'RGB')
-    #         cam_name = '{}-{}layers'.format(args.cam, len(model_target_layer))
-    #         masked_img.save(os.path.join(dest, cam_name+'.jpg'))
+            # swap the axis so that the show_cam_on_image works
+            img = x[i, :].cpu().detach().numpy()
+            img = np.swapaxes(img, 0, 2)
+            img = np.swapaxes(img, 0, 1)
+
+            # save the overlayed-attention map with the cam name as a tag
+            attention_map = show_cam_on_image(img, grayscale_cam[i, :], use_rgb=True)
+            masked_img = Image.fromarray(attention_map, 'RGB')
+            cam_name = '{}-{}layers'.format(args.cam, len(model_target_layer))
+            masked_img.save(os.path.join(dest, cam_name+'.jpg'))
+
+            # update the sequential index for next iterations
+            img_index += 1
